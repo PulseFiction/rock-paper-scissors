d932e4a4a01dd2d303efec614c52733f8b67864e
Sebastian <seb-ldn@hotmail.co.uk>
2021-10-27 10:40:19 +0100

    Added UI and scripting for feature function.

--- script.js
+++ script.js
@@ -11,80 +11,123 @@ function getRandom(max) {
     return Math.floor(Math.random() * max);
 }
 
-
-// console.log(getRandom(3));
-
-
 function computerPlay(){
-    let cpuRock = 'Rock'
-    let cpuScissors = 'Scissors'
-    let cpuPaper = 'Paper'
-    
-    // getRandom(3) - Don't have to call the function for it to work
+    let cpuRock = 'Rock';
+    let cpuScissors = 'Scissors';
+    let cpuPaper = 'Paper';
     
     if (getRandom(3) === 0) {
         return cpuRock;
     } else if (getRandom(3) === 1) {
-        return cpuScissors
+        return cpuScissors;
     } else {
-        return cpuPaper
+        return cpuPaper;
     }
+};
 
-    
-}
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const display = document.querySelector('.result')
 
-function playRound() {
-    let choice = prompt('Choose Rock, Paper or Scissors!')
+
+
+
+rock.addEventListener('click', () => {
     let cpuChoice = computerPlay();
-    choice = choice.toLowerCase()
-    console.log(`You have chosen ${choice}! The computer has
-    chosen ${cpuChoice}!`)
+    if (cpuChoice === 'Scissors') {
+        answer = 'CPU picked Scissors! Rock beats Scissors, you win!';
+    } else if (cpuChoice === 'Paper') {
+         answer = 'CPU picked Paper! Paper covers Rock, you lose!';
+    } else {
+         answer = 'CPU picked Rock! It\'s a draw!'
+    }
+    display.textContent = answer;
+});
 
-    if (choice == 'rock' && cpuChoice === 'Scissors') {
-        let answer = 'Rock beats Scissors, you won!'
-        return answer;
-    } else if (choice == 'scissors' && cpuChoice === 'Rock'){
-        let answer ='Rock beats Scissors, you lose!'
-        return answer;
-    } else if (choice == 'scissors' && cpuChoice === 'Paper'){
-        let answer = 'Scissors beats Paper, you won!'
-        return answer;
-    } else if (choice == 'paper' && cpuChoice === 'Scissors'){
-        let answer = 'Scissors beats Paper, you lose!'
-        return answer;
-    } else if (choice == 'paper' && cpuChoice === 'Rock'){
-        let answer = 'Paper covers Rock, you win!'
-        return answer;
-    } else if (choice == 'rock' && cpuChoice === 'Paper'){
-        let answer = 'Paper covers Rock, you lose!'
-        return answer;
+
+paper.addEventListener('click', () => {
+    
+    let cpuChoice = computerPlay();
+    if (cpuChoice === 'Rock') {
+        answer = 'CPU picked Rock! Paper covers Rock, you win!';
+    } else if (cpuChoice === 'Scissors') {
+        answer = 'CPU picked Scissors! Scissors cuts Paper, you lose!';
+
+    } else {
+        answer = 'CPU picked Paper! It\'s a draw!';
+    }
+    display.textContent = answer;
+});
+
+
+scissors.addEventListener('click', () => {
+    
+    let cpuChoice = computerPlay();
+    if (cpuChoice === 'Paper') {
+        answer = 'CPU picked Paper! Scissors cuts Paper, you win!';
+    } else if (cpuChoice === 'Scissors') {
+        answer = 'CPU picked Rock! Rock crushes Scissors, you lose!';
+
+    } else {
+        answer = 'CPU picked Scissors! It\'s a draw!';
+    }
+    display.textContent = answer;
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function playRound(rock, paper, scissors) {
+    let cpuChoice = computerPlay();
+    let choice = 
+    
+    if (choice === rock && cpuChoice === 'Scissors') {
+        answer = 'Rock beats Scissors, you won!'
+         
+    } else if (choice == scissors && cpuChoice === 'Rock'){
+        answer ='Rock beats Scissors, you lose!'
+        
+    } else if (choice == scissors && cpuChoice === 'Paper'){
+        answer = 'Scissors beats Paper, you won!'
+        
+    } else if (choice == paper && cpuChoice === 'Scissors'){
+        answer = 'Scissors beats Paper, you lose!'
+        
+    } else if (choice == paper && cpuChoice === 'Rock'){
+        answer = 'Paper covers Rock, you win!'
+        
+    } else if (choice == rock && cpuChoice === 'Paper'){
+        answer = 'Paper covers Rock, you lose!'
+
     } else if (choice === cpuChoice) {
-        return 'Same choice! Go again!'
+        answer = 'Same choice! Go again!'
     }
     else {
-        return 'Please enter a valid choice.'
+        answer = 'Please enter a valid choice.'
     }
+    console.log(answer);
+}; */
 
-    
-    
-}
 
-console.log(playRound())
 
-function game() {
-    let rounds = 5;
-    let score = 0;
-    let cpuScore = 0;
-    for (let i = 0 ; i < rounds ; i++) {
-        playRound()
-        if (answer.includes('lose')){
-            cpuScore += 1;
-        } else if (answer.includes('win')){
-            score += 1;
-        } 
 
-    }
-    console.log(`The final score is: You - ${score} points! CPU - ${cpuScore} points!`)
-}
 
-game();
+
